d7c8373f6aa50e190d7ce934ce9081af2f02a43f
HarryNicholls <harry.nicholls@hotmail.com>
2016-11-21 16:23:27 +0000

    connection script push 1

---
+++ connect.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 1405741 on 21/11/2016.
+ */
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+//and our HTTP server
+var http = require('http');
+//setup our port
+var port = process.env.PORT || 1337;
